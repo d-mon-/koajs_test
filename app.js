7bff560280c8d41baddccae71b76d5f553a72c74
olivier <guerin.olivier@outlook.com>
2015-09-09 14:06:26 +0200

    'test middleware'

---
+++ app.js
@@ -0,0 +1,31 @@
+/**
+ * Created by GUERIN Olivier, on 09/09/2015.
+ * Twitter: @MisterRaton
+ */
+
+var koa = require('koa');
+var app = koa();
+
+
+app.use(function *(next) {
+    "use strict";
+    console.log(this);
+    console.log('1');
+    yield next;
+    var date = new Date();
+    console.log(`5 ${date}`);
+
+});
+
+app.use(function *(next){
+    "use strict";
+    console.log('2');
+    yield next;
+    console.log('4');
+});
+
+app.use(function *() {
+    console.log('3');
+    this.body = 'hello World';
+});
+app.listen(3000);
